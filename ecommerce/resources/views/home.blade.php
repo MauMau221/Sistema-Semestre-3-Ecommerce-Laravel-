f05b4bd4bb98d6d28e26cc2455dca8b920cf9158
MauMau221 <mauriciobgsp@gmail.com>
2025-03-16 20:12:20 -0300

    ajustando estilização do card produto

--- ecommerce/resources/views/home.blade.php
+++ ecommerce/resources/views/home.blade.php
@@ -15,182 +15,80 @@
         <div class="container">
             {{-- Categorias --}}
             <div class="row justify-content-center">
-                <div class="col-4 col-lg-4 col-md-4 col-sm-6
+                <div class="col-12 col-lg-4 col-md-4 col-sm-6
                 my-2">
-                    <div class="card text-center bg-light">
-                        <a href="#" class="position-absolute custom-position p-3 text-dark">
+                    <div class="card text-center">
+                        <a href="#" class="position-absolute custom-position">
                         </a>
                         <img src="/css/image/categorias/catCamisas.png" class="card-img-top">
                     </div>
                 </div>
-                <div class="col-4 col-lg-4 col-md-4 col-sm-6
+                <div class="col-12 col-lg-4 col-md-4 col-sm-6
                 my-2">
-                    <div class="card text-center bg-light">
-                        <a href="#" class="position-absolute custom-position p-3 text-dark">
+                    <div class="card text-center">
+                        <a href="#" class="position-absolute custom-position">
                         </a>
                         <img src="/css/image/categorias/catCamisas1.png" class="card-img-top">
                     </div>
                 </div>
-                <div class="col-4 col-lg-4 col-md-4 col-sm-6
+                <div class="col-12 col-lg-4 col-md-4 col-sm-6
                 my-2">
-                    <div class="card text-center bg-light">
-                        <a href="#" class="position-absolute custom-position p-3 text-dark">
+                    <div class="card text-center">
+                        <a href="#" class="position-absolute custom-position">
                         </a>
                         <img src="/css/image/categorias/catCamisas2.png" class="card-img-top">
                     </div>
                 </div>
-                <div class="col-4 col-lg-4 col-md-4 col-sm-6
+                <div class="col-12 col-lg-4 col-md-4 col-sm-6
                 my-2">
-                    <div class="card text-center bg-light">
-                        <a href="#" class="position-absolute custom-position p-3 text-dark">
+                    <div class="card text-center">
+                        <a href="#" class="position-absolute custom-position">
                         </a>
                         <img src="/css/image/categorias/catCamisas3.png" class="card-img-top">
                     </div>
                 </div>
-                <div class="col-4 col-lg-4 col-md-4 col-sm-6
+                <div class="col-12 col-lg-4 col-md-4 col-sm-6
                 my-2">
-                    <div class="card text-center bg-light">
-                        <a href="#" class="position-absolute custom-position p-3 text-dark">
+                    <div class="card text-center">
+                        <a href="#" class="position-absolute custom-position">
                         </a>
                         <img src="/css/image/categorias/catCamisas4.png" class="card-img-top">
                     </div>
                 </div>
-                <div class="col-4 col-lg-4 col-md-4 col-sm-6
+                <div class="col-12 col-lg-4 col-md-4 col-sm-6
                 my-2">
-                    <div class="card text-center bg-light">
-                        <a href="#" class="position-absolute custom-position p-3 text-dark">
+                    <div class="card text-center">
+                        <a href="#" class="position-absolute custom-position">
                         </a>
                         <img src="/css/image/categorias/catCamisas5.png" class="card-img-top">
                     </div>
                 </div>
             </div>
             <hr class="mt-3">
+
+
+
+
             <div class="row justify-content-center">
                 <div class="col-xl-3 col-lg-3 col-md-4 col-sm-6
                 my-2">
-                    <div class="text-center bg-light">
-                        <a href="#" class="position-absolute custom-position p-3 text-dark">
-                            <i class="fa-regular fa-heart fa-2xl"></i>
-                        </a>
-                        <img src="/css/image/card/image.png" class="card-img-top">
-                        <div class="">
-                            <h5 class="card-title">Um produto qualquer</h5>
-                            <p class="card-text encurtar-3l">Descrição meramente para distração e sem contexto algum</p>
-                        </div>
-                        <div class="">
-                            <div>
-                                <button class="btn btn-light">
-                                    <small>Adicionar ao carrinho</small>
-                                </button>
+                    <div>
+                        <div class="card-product">
+                            <img src="/css/image/card/image.png" class="card-img-top">
+                            <div class="card d-flex flex-column p-2 border-0">
+                                <div class="star">
+                                    <i class="fa-solid fa-star fa-2xs"></i>
+                                    <i class="fa-solid fa-star fa-2xs"></i>
+                                    <i class="fa-solid fa-star fa-2xs"></i>
+                                    <i class="fa-solid fa-star fa-2xs"></i>
+                                    <i class="fa-regular fa-star fa-2xs"></i>
+                                </div>
+                                <h5 class="font-weight-bold">CAMISA SLIM TRICONE MAQUINETADA BRANCA</h5>
+                                <p class="card-price">
+                                    <s>R$399,99</s> 
+                                    <strong>199,99</strong>
+                                </p>
                             </div>
-                            <small class="text-success">32 Unidades</small>
-                        </div>
-                    </div>
-                </div>
-                <div class="col-xl-3 col-lg-3 col-md-4 col-sm-6
-                my-2">
-                    <div class="card text-center bg-light">
-                        <a href="#" class="position-absolute custom-position p-3 text-dark">
-                            <i class="fa-regular fa-heart fa-2xl"></i>
-                        </a>
-                        <img src="/css/image/card/image.png" class="card-img-top">
-                        <div class="card-body">
-                            <h5 class="card-title">Um produto qualquer</h5>
-                            <p class="card-text encurtar-3l">Descrição meramente para distração e sem contexto algum</p>
-                        </div>
-                        <div class="card-footer">
-                            <div>
-                                <button class="btn btn-light">
-                                    <small>Adicionar ao carrinho</small>
-                                </button>
-                            </div>
-                            <small class="text-success">32 Unidades</small>
-                        </div>
-                    </div>
-                </div>
-                <div class="col-xl-3 col-lg-3 col-md-4 col-sm-6
-                my-2">
-                    <div class="card text-center bg-light">
-                        <a href="#" class="position-absolute custom-position p-3 text-dark">
-                            <i class="fa-regular fa-heart fa-2xl"></i>
-                        </a>
-                        <img src="/css/image/card/image.png" class="card-img-top">
-                        <div class="card-body">
-                            <h5 class="card-title">Um produto qualquer</h5>
-                            <p class="card-text encurtar-3l">Descrição meramente para distração e sem contexto algum</p>
-                        </div>
-                        <div class="card-footer">
-                            <div>
-                                <button class="btn btn-light">
-                                    <small>Adicionar ao carrinho</small>
-                                </button>
-                            </div>
-                            <small class="text-success">32 Unidades</small>
-                        </div>
-                    </div>
-                </div>
-
-                <div class="col-xl-3 col-lg-3 col-md-4 col-sm-6
-                my-2">
-                    <div class="card text-center bg-light">
-                        <a href="#" class="position-absolute custom-position p-3 text-dark">
-                            <i class="fa-regular fa-heart fa-2xl"></i>
-                        </a>
-                        <img src="/css/image/card/image.png" class="card-img-top">
-                        <div class="card-body">
-                            <h5 class="card-title">Um produto qualquer</h5>
-                            <p class="card-text encurtar-3l">Descrição meramente para distração e sem contexto algum</p>
-                        </div>
-                        <div class="card-footer">
-                            <div>
-                                <button class="btn btn-light">
-                                    <small>Adicionar ao carrinho</small>
-                                </button>
-                            </div>
-                            <small class="text-success">32 Unidades</small>
-                        </div>
-                    </div>
-                </div>
-                <div class="col-xl-3 col-lg-3 col-md-4 col-sm-6
-                my-2">
-                    <div class="card text-center bg-light">
-                        <a href="#" class="position-absolute custom-position p-3 text-dark">
-                            <i class="fa-regular fa-heart fa-2xl"></i>
-                        </a>
-                        <img src="/css/image/card/image.png" class="card-img-top">
-                        <div class="card-body">
-                            <h5 class="card-title">Um produto qualquer</h5>
-                            <p class="card-text encurtar-3l">Descrição meramente para distração e sem contexto algum</p>
-                        </div>
-                        <div class="card-footer">
-                            <div>
-                                <button class="btn btn-light">
-                                    <small>Adicionar ao carrinho</small>
-                                </button>
-                            </div>
-                            <small class="text-success">32 Unidades</small>
-                        </div>
-                    </div>
-                </div>
-                <div class="col-xl-3 col-lg-3 col-md-4 col-sm-6
-                my-2">
-                    <div class="card text-center bg-light">
-                        <a href="#" class="position-absolute custom-position p-3 text-dark">
-                            <i class="fa-regular fa-heart fa-2xl"></i>
-                        </a>
-                        <img src="/css/image/card/image.png" class="card-img-top">
-                        <div class="card-body">
-                            <h5 class="card-title">Um produto qualquer</h5>
-                            <p class="card-text encurtar-3l">Descrição meramente para distração e sem contexto algum
-                            </p>
-                        </div>
-                        <div class="card-footer">
-                            <div>
-                                <button class="btn btn-light">
-                                    <small>Adicionar ao carrinho</small>
-                                </button>
-                            </div>
-                            <small class="text-success">32 Unidades</small>
                         </div>
                     </div>
                 </div>
