92a90a96fd3addff66c0e96deeba7156a2a2370c
MauMau221 <mauriciobgsp@gmail.com>
2025-03-17 21:55:00 -0300

    Feito os dados do banco aparecer nos cards da home

--- ecommerce/app/Http/Controllers/HomeController.php
+++ ecommerce/app/Http/Controllers/HomeController.php
@@ -2,20 +2,17 @@
 
 namespace App\Http\Controllers;
 
-use App\Models\Categoria;
 use App\Models\Produto;
 
 class HomeController extends Controller
 {
     public function index()
     {
-        $produto = Produto::find(2);
-        $categoria = $produto->categoria;
-        dd($categoria);
+        $itens = Produto::all();
 
         return view('home', ['itens' => $itens]);
     }
-
+    
     public function login()
     {
         return view('login.login');
--- ecommerce/resources/views/home.blade.php
+++ ecommerce/resources/views/home.blade.php
@@ -69,210 +69,42 @@
             <h3 class="p-4">Camisas</h3>
 
             <div class="container">
-                <div class="d-flex justify-content-end m-2"> 
-                    <div class="button-prev p-1"><i class="fa-solid fa-circle-chevron-right fa-flip-horizontal fa-2xl"></i></div>
+                <div class="d-flex justify-content-end m-2">
+                    <div class="button-prev p-1"><i class="fa-solid fa-circle-chevron-right fa-flip-horizontal fa-2xl"></i>
+                    </div>
                     <div class="button-next p-1"><i class="fa-solid fa-circle-chevron-right fa-2xl"></i></div>
                 </div>
                 <div class="swiper mySwiper">
                     <div class="swiper-wrapper">
-                        <div class="swiper-slide">
-                            <div class="card-product">
-                                <div class="image-container position-relative">
-                                    <img src="/css/image/card/image.png" class="card-img-top">
-                                    <a href="#" class="p-3 text-dark cart-icon position-absolute bottom-0 end-0">
-                                        <i class="fa-solid fa-cart-plus fa-xl"></i>
-                                    </a>
-                                </div>
-                                <div class="card d-flex flex-column p-2 border-0">
-                                    <div class="star">
-                                        <i class="fa-solid fa-star fa-2xs"></i>
-                                        <i class="fa-solid fa-star fa-2xs"></i>
-                                        <i class="fa-solid fa-star fa-2xs"></i>
-                                        <i class="fa-solid fa-star fa-2xs"></i>
-                                        <i class="fa-regular fa-star fa-2xs"></i>
+                        @foreach ($itens as $item)
+                            <div class="swiper-slide">
+                                <div class="card-product">
+                                    <div class="image-container position-relative">
+                                        <img src="/css/image/card/image.png" class="card-img-top">
+                                        <a href="#" class="p-3 text-dark cart-icon position-absolute bottom-0 end-0">
+                                            <i class="fa-solid fa-cart-plus fa-xl"></i>
+                                        </a>
                                     </div>
-                                    <h5 class="font-weight-bold">CAMISA SLIM TRICONE MAQUINETADA BRANCA</h5>
-                                    <p class="card-price">
-                                        <s>R$399,99</s>
-                                        <strong>199,99</strong>
-                                    </p>
+                                    <div class="card d-flex flex-column p-2 border-0">
+                                        <div class="star">
+                                            <i class="fa-solid fa-star fa-2xs"></i>
+                                            <i class="fa-solid fa-star fa-2xs"></i>
+                                            <i class="fa-solid fa-star fa-2xs"></i>
+                                            <i class="fa-solid fa-star fa-2xs"></i>
+                                            <i class="fa-regular fa-star fa-2xs"></i>
+                                        </div>
+                                        <h5 class="font-weight-bold">{{ $item['nome'] }}</h5>
+                                        <p class="card-price">
+                                            <s>R$399,99</s>
+                                            <strong>{{ $item['preco'] }}</strong>
+                                        </p>
+                                    </div>
                                 </div>
                             </div>
-                        </div>
-                        <div class="swiper-slide">
-                            <div class="card-product">
-                                <div class="image-container position-relative">
-                                    <img src="/css/image/card/image.png" class="card-img-top">
-                                    <a href="#" class="p-3 text-dark cart-icon position-absolute bottom-0 end-0">
-                                        <i class="fa-solid fa-cart-plus fa-xl"></i>
-                                    </a>
-                                </div>
-                                <div class="card d-flex flex-column p-2 border-0">
-                                    <div class="star">
-                                        <i class="fa-solid fa-star fa-2xs"></i>
-                                        <i class="fa-solid fa-star fa-2xs"></i>
-                                        <i class="fa-solid fa-star fa-2xs"></i>
-                                        <i class="fa-solid fa-star fa-2xs"></i>
-                                        <i class="fa-regular fa-star fa-2xs"></i>
-                                    </div>
-                                    <h5 class="font-weight-bold">CAMISA SLIM TRICONE MAQUINETADA BRANCA</h5>
-                                    <p class="card-price">
-                                        <s>R$399,99</s>
-                                        <strong>199,99</strong>
-                                    </p>
-                                </div>
-                            </div>
-                        </div>
-                        <div class="swiper-slide">
-                            <div class="card-product">
-                                <div class="image-container position-relative">
-                                    <img src="/css/image/card/image.png" class="card-img-top">
-                                    <a href="#" class="p-3 text-dark cart-icon position-absolute bottom-0 end-0">
-                                        <i class="fa-solid fa-cart-plus fa-xl"></i>
-                                    </a>
-                                </div>
-                                <div class="card d-flex flex-column p-2 border-0">
-                                    <div class="star">
-                                        <i class="fa-solid fa-star fa-2xs"></i>
-                                        <i class="fa-solid fa-star fa-2xs"></i>
-                                        <i class="fa-solid fa-star fa-2xs"></i>
-                                        <i class="fa-solid fa-star fa-2xs"></i>
-                                        <i class="fa-regular fa-star fa-2xs"></i>
-                                    </div>
-                                    <h5 class="font-weight-bold">CAMISA SLIM TRICONE MAQUINETADA BRANCA</h5>
-                                    <p class="card-price">
-                                        <s>R$399,99</s>
-                                        <strong>199,99</strong>
-                                    </p>
-                                </div>
-                            </div>
-                        </div>
-                        <div class="swiper-slide">
-                            <div class="card-product">
-                                <div class="image-container position-relative">
-                                    <img src="/css/image/card/image.png" class="card-img-top">
-                                    <a href="#" class="p-3 text-dark cart-icon position-absolute bottom-0 end-0">
-                                        <i class="fa-solid fa-cart-plus fa-xl"></i>
-                                    </a>
-                                </div>
-                                <div class="card d-flex flex-column p-2 border-0">
-                                    <div class="star">
-                                        <i class="fa-solid fa-star fa-2xs"></i>
-                                        <i class="fa-solid fa-star fa-2xs"></i>
-                                        <i class="fa-solid fa-star fa-2xs"></i>
-                                        <i class="fa-solid fa-star fa-2xs"></i>
-                                        <i class="fa-regular fa-star fa-2xs"></i>
-                                    </div>
-                                    <h5 class="font-weight-bold">CAMISA SLIM TRICONE MAQUINETADA BRANCA</h5>
-                                    <p class="card-price">
-                                        <s>R$399,99</s>
-                                        <strong>199,99</strong>
-                                    </p>
-                                </div>
-                            </div>
-                        </div>
-                        <div class="swiper-slide">
-                            <div class="card-product">
-                                <div class="image-container position-relative">
-                                    <img src="/css/image/card/image.png" class="card-img-top">
-                                    <a href="#" class="p-3 text-dark cart-icon position-absolute bottom-0 end-0">
-                                        <i class="fa-solid fa-cart-plus fa-xl"></i>
-                                    </a>
-                                </div>
-                                <div class="card d-flex flex-column p-2 border-0">
-                                    <div class="star">
-                                        <i class="fa-solid fa-star fa-2xs"></i>
-                                        <i class="fa-solid fa-star fa-2xs"></i>
-                                        <i class="fa-solid fa-star fa-2xs"></i>
-                                        <i class="fa-solid fa-star fa-2xs"></i>
-                                        <i class="fa-regular fa-star fa-2xs"></i>
-                                    </div>
-                                    <h5 class="font-weight-bold">CAMISA SLIM TRICONE MAQUINETADA BRANCA</h5>
-                                    <p class="card-price">
-                                        <s>R$399,99</s>
-                                        <strong>199,99</strong>
-                                    </p>
-                                </div>
-                            </div>
-                        </div>
-                        <div class="swiper-slide">
-                            <div class="card-product">
-                                <div class="image-container position-relative">
-                                    <img src="/css/image/card/image.png" class="card-img-top">
-                                    <a href="#" class="p-3 text-dark cart-icon position-absolute bottom-0 end-0">
-                                        <i class="fa-solid fa-cart-plus fa-xl"></i>
-                                    </a>
-                                </div>
-                                <div class="card d-flex flex-column p-2 border-0">
-                                    <div class="star">
-                                        <i class="fa-solid fa-star fa-2xs"></i>
-                                        <i class="fa-solid fa-star fa-2xs"></i>
-                                        <i class="fa-solid fa-star fa-2xs"></i>
-                                        <i class="fa-solid fa-star fa-2xs"></i>
-                                        <i class="fa-regular fa-star fa-2xs"></i>
-                                    </div>
-                                    <h5 class="font-weight-bold">CAMISA SLIM TRICONE MAQUINETADA BRANCA</h5>
-                                    <p class="card-price">
-                                        <s>R$399,99</s>
-                                        <strong>199,99</strong>
-                                    </p>
-                                </div>
-                            </div>
-                        </div>
-                        <div class="swiper-slide">
-                            <div class="card-product">
-                                <div class="image-container position-relative">
-                                    <img src="/css/image/card/image.png" class="card-img-top">
-                                    <a href="#" class="p-3 text-dark cart-icon position-absolute bottom-0 end-0">
-                                        <i class="fa-solid fa-cart-plus fa-xl"></i>
-                                    </a>
-                                </div>
-                                <div class="card d-flex flex-column p-2 border-0">
-                                    <div class="star">
-                                        <i class="fa-solid fa-star fa-2xs"></i>
-                                        <i class="fa-solid fa-star fa-2xs"></i>
-                                        <i class="fa-solid fa-star fa-2xs"></i>
-                                        <i class="fa-solid fa-star fa-2xs"></i>
-                                        <i class="fa-regular fa-star fa-2xs"></i>
-                                    </div>
-                                    <h5 class="font-weight-bold">CAMISA SLIM TRICONE MAQUINETADA BRANCA</h5>
-                                    <p class="card-price">
-                                        <s>R$399,99</s>
-                                        <strong>199,99</strong>
-                                    </p>
-                                </div>
-                            </div>
-                        </div>
+                        @endforeach
+
                     </div>
                 </div>
-
-
-
-                @foreach ($itens as $item)
-                    <div class="col-xl-3 col-lg-3 col-md-4 col-sm-6 my-1">
-                        <div class="card text-center bg-light">
-                            <a href="#" class="position-absolute custom-position p-3 text-dark">
-                                <i class="fa-regular fa-heart fa-2xl"></i>
-                            </a>
-                            <img src="/css/image/card/image.png" class="card-img-top">
-                            <div class="card-header">
-                                R${{ number_format($item['preco'], 2, ',', ',') }}
-                            </div>
-                            <div class="card-body">
-                                <h5 class="card-title">{{ $item['nome'] }}</h5>
-                                <p class="card-text encurtar-3l">{{ $item['desc'] }}</p>
-                            </div>
-                            <div class="card-footer">
-                                <form action="" class="d-block">
-                                    <button class="btn btn-danger btn-sm">
-                                        Adicionar ao Carrinho
-                                    </button>
-                                </form>
-                                <small class="text-success">320,5kg em estoque</small>
-                            </div>
-                        </div>
-                    </div>
-                @endforeach
             </div>
             <hr class="mt-3">
             <div class="row pb-4">
